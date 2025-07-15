ffba7070bc199a6f175bc586cf75144805d54883
poach3r <poacherGoneWild@proton.me>
2025-07-14 22:55:58 -0400

    Add rust-analyzer to flake

--- flake.nix
+++ flake.nix
@@ -5,18 +5,23 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        naersk-lib = pkgs.callPackage naersk {};
+      in {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       }
     );
 }
