7c09be9bd4e88437e21abc4cba1f80a86a31662a
poach3r <poacherGoneWild@proton.me>
2025-07-15 13:34:20 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,12 +15,23 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
         naersk-lib = pkgs.callPackage naersk {};
+        libPath = with pkgs;
+          lib.makeLibraryPath [
+            libGL
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ];
       in {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs;
           mkShell {
-            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer];
+            buildInputs = [cargo rustc rustfmt rust-analyzer pre-commit rustPackages.clippy];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            LD_LIBRARY_PATH = libPath;
           };
       }
     );
